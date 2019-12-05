97f00908d3eb0a196552f8f935044935c2d3f221
팡식 <kwang20@plani.co.kr>
2019-12-05 17:20:14 +0900

    vuex-test

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="id">
+    Parent counter : {{ this.$store.state.counter }} <br>
+    <button @click="addCounter">+</button>
+    <button @click="subCounter">-</button>
+    <!-- <child v-bind:passedCounter="counter"></child> -->
+    <child></child>
+  </div>
+</template>
+
+<script>
+import Child from './Child.vue'
+
+export default {
+  data () {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    addCounter() {
+      this.$store.state.counter++;
+    },
+    subCounter() {
+      this.$store.state.counter--;
+    }
+  },
+  components: {
+    'child': Child
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/Child.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <hr>
+        Child counter : {{ this.$store.state.counter }} <br>
+        <button>+</button>
+        <button>-</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['passedCounter']
+}
+</script>
+
+<style>
+
+</style>
